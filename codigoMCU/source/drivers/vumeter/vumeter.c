d32834d542c4d87b0f7a67b62f6d9fd2cb798d31
cmeichtry <cmeichtry@itba.edu.ar>
2025-05-20 18:50:51 -0300

    Discretización de frecuencias y niveles de volumen

--- codigoMCU/source/drivers/vumeter/vumeter.c
+++ codigoMCU/source/drivers/vumeter/vumeter.c
@@ -3,7 +3,7 @@
    @brief    vumeter driver functions
    @author   Grupo 3
   ******************************************************************************/
- 
+
 /*******************************************************************************
  * INCLUDE HEADER FILES
  ******************************************************************************/
@@ -15,6 +15,40 @@
 #include "../SDK/CMSIS/MK64F12.h"
 
 /*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+/**
+ * @brief Convert a frequency to a row number
+ *
+ * @param value
+ * @return The row number corresponding to the frequency
+ */
+static uint8_t setRow(float value);
+
+/**
+ * @brief Convert a value (level) to a column number
+ *
+ * @param frequency The frequency to convert
+ * @return The column number corresponding to the value
+ */
+static uint8_t setColumn(uint16_t frequency);
+
+/*******************************************************************************
+ * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+uint16_t vumeter_frequencies[VUMETER_FREQUENCIES] = {
+    VUMETER_FREQUENCY_00,
+    VUMETER_FREQUENCY_01,
+    VUMETER_FREQUENCY_02,
+    VUMETER_FREQUENCY_03,
+    VUMETER_FREQUENCY_04,
+    VUMETER_FREQUENCY_05,
+    VUMETER_FREQUENCY_06,
+    VUMETER_FREQUENCY_07,
+    VUMETER_FREQUENCY_08,
+    VUMETER_FREQUENCY_09};
+
+/*******************************************************************************
  * INITIALIZATION FUNCTION
  ******************************************************************************/
 bool vumeter_init(void)
@@ -23,8 +57,8 @@ bool vumeter_init(void)
     if (!initialized)
     {
         // tengo que inicializar la matriz, la concha del pato
-        //matrix_init();
-        //matrix_full_screen();
+        // matrix_init();
+        // matrix_full_screen();
 
         initialized = true;
     }
@@ -35,20 +69,73 @@ bool vumeter_init(void)
  * FUNCTION DEFINITIONS
  ******************************************************************************/
 
-void vumeter_set_frequency(vumeter_frequency_t frequency, uint8_t value){
-
+void vumeter_set_frequency(vumeter_frequency_t frequency, uint8_t value)
+{
 }
 
-void vumeter_set_all_band(uint8_t value){
-
+void vumeter_set_all_band(uint16_t value)
+{
 }
 
-
-void vumeter_clear(void){
-
+void vumeter_clear(void)
+{
 }
 
+void vumeter_set_brightness(uint8_t brightness)
+{
+}
 
-void vumeter_set_brightness(uint8_t brightness){
-    
+static uint8_t setRow(float value)
+{
+    value = value * ((float)MATRIX_HEIGHT / VUMETER_MAX);
+    value = (uint8_t)(value + 0.5f);
+
+    if (value >= MATRIX_HEIGHT)
+        value = MATRIX_HEIGHT - 1;
+
+    return value;
+}
+
+static uint8_t setColumn(uint16_t frequency)
+{
+    uint8_t low = 0;
+    uint8_t high = VUMETER_FREQUENCIES - 1;
+    uint8_t mid = (low + high) / 2;
+
+    if (frequency <= VUMETER_FREQUENCY_00)
+    {
+        return = 0;
+    }
+    else if (frequency >= VUMETER_FREQUENCY_09)
+    {
+        return = VUMETER_FREQUENCIES - 1;
+    }
+
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if (vumeter_frequencies[mid] == frequency)
+        {
+            return mid;
+        }
+        else if (vumeter_frequencies[mid] < frequency)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if(low >= VUMETER_FREQUENCIES)
+        return VUMETER_FREQUENCIES - 1;
+
+    if(low == 0)
+        return 0;
+
+    if ((vumeter_frequencies[low] - frequency) < (frequency - vumeter_frequencies[low - 1]))
+        return low;
+    else
+        return low - 1;
 }
--- codigoMCU/source/drivers/vumeter/vumeter.h
+++ codigoMCU/source/drivers/vumeter/vumeter.h
@@ -19,57 +19,63 @@
 
 #define VUMETER_MIN 0
 #define VUMETER_MAX 100
+#define VUMETER_FREQUENCIES 10
 
+#define MATRIX_HEIGHT 8
+#define MATRIX_WIDTH 8
+#define MATRIX_PIXELS (MATRIX_WIDTH * MATRIX_HEIGHT)
 /*******************************************************************************
  * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
  ******************************************************************************/
 typedef enum
 {
-    VUMETER_FREQUENCY_20HZ = 20,
-    VUMETER_FREQUENCY_50HZ = 50,
-    VUMETER_FREQUENCY_100HZ,
-    VUMETER_FREQUENCY_200HZ,
-    VUMETER_FREQUENCY_400HZ,
-    VUMETER_FREQUENCY_800HZ,
-    VUMETER_FREQUENCY_1600HZ,
-    VUMETER_FREQUENCY_3200HZ
+  VUMETER_FREQUENCY_00 = 20,
+  VUMETER_FREQUENCY_01 = 50,
+  VUMETER_FREQUENCY_02 = 100,
+  VUMETER_FREQUENCY_03 = 200,
+  VUMETER_FREQUENCY_04 = 400,
+  VUMETER_FREQUENCY_05 = 800,
+  VUMETER_FREQUENCY_06 = 1600,
+  VUMETER_FREQUENCY_07 = 3200,
+  VUMETER_FREQUENCY_08 = 6400,
+  VUMETER_FREQUENCY_09 = 12800,
 } vumeter_frequency_t;
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 
- /**
-  * @brief Initialize the vumeter
-  * 
-  * @return true if initialization was successful, false otherwise
-  */
+/**
+ * @brief Initialize the vumeter
+ *
+ * @return true if initialization was successful, false otherwise
+ */
 bool vumeter_init(void);
 
 /**
  * @brief Set the vumeter frequency
- * 
+ *
  * @param frequency The frequency to set
  * @param value The value to set for the frequency (between VUMETER_MIN and VUMETER_MAX)
  */
-void vumeter_set_frequency(vumeter_frequency_t frequency, uint8_t value);
+void vumeter_set_frequency(vumeter_frequency_t frequency, uint16_t value);
 
 /**
  * @brief Set the vumeter value for all bands
- * 
+ *
  * @param value The value to set for the frequency (between VUMETER_MIN and VUMETER_MAX)
  */
-void vumeter_set_all_band(uint8_t value);
+void vumeter_set_all_band(uint16_t value);
 
 /**
  * @brief clear the vumeter display, turn off all LEDs
- * 
+ *
  */
 void vumeter_clear(void);
 
 /**
  * @brief Set the brightness of the vumeter
- * 
+ *
  * @param brightness level of brightness (0-100)
  */
 void vumeter_set_brightness(uint8_t brightness);
